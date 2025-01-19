9d38bc73994953c718dde70a9ed1935f07eeffdb
Henrique Miranda <henriquepw4@gmail.com>
2025-01-18 21:27:11 -0300

    fix reanimated warning

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,11 +3,20 @@ import "../../assets/global.css"
 import { Stack } from "expo-router"
 import { useColorScheme } from "nativewind"
 import { useEffect } from "react"
+import {
+  ReanimatedLogLevel,
+  configureReanimatedLogger,
+} from "react-native-reanimated"
 import { GluestackUIProvider } from "~/components/ui/gluestack-ui-provider"
 import { StoreProvider } from "../store"
 import { useTheme } from "../store/theme-store"
 
 const opts = { headerShown: false }
+
+configureReanimatedLogger({
+  level: ReanimatedLogLevel.warn,
+  strict: false,
+})
 
 export default function RootLayout() {
   const { setColorScheme } = useColorScheme()
@@ -24,6 +33,10 @@ export default function RootLayout() {
           <Stack.Screen name="(tabs)" />
           <Stack.Screen
             name="transations/register"
+            options={{ presentation: "modal" }}
+          />
+          <Stack.Screen
+            name="recurrents/register"
             options={{ presentation: "modal" }}
           />
         </Stack>
