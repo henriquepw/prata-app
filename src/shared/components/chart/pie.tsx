290bc9d7d1e7963f38bf94c4e33ad1147f4938e6
Henrique Miranda <henriquepw4@gmail.com>
2025-09-06 17:16:46 -0300

    fix: nan angle

--- src/shared/components/chart/pie.tsx
+++ src/shared/components/chart/pie.tsx
@@ -1,5 +1,6 @@
 import { Canvas, Path, Skia } from "@shopify/react-native-skia"
 import { useMemo } from "react"
+import { useTheme } from "~/shared/store/theme"
 import { Box } from "../box"
 
 const GAP = 0.055
@@ -21,15 +22,29 @@ type Props = {
 export function PieChart({ children, radius, data, strokeWidth }: Props) {
   const size = radius * 2
   const innerRadius = radius - strokeWidth
+  const theme = useTheme()
   const paths = useMemo(() => {
     const path = Skia.Path.Make()
     path.addCircle(radius, radius, innerRadius)
 
-    const paths = []
+    const paths = [
+      <Path
+        color={theme === "dark" ? "rgba(255,255,255,0.1)" : "rgba(0,0,0,0.1)"}
+        key="default"
+        path={path}
+        start={0}
+        end={360}
+        strokeCap="round"
+        strokeWidth={strokeWidth}
+        style="stroke"
+      />,
+    ]
+
     let startAngle = 0
     for (let i = 0; i < data.length; i++) {
       const d = data[i]
-      const endAngle = startAngle + Number(d.percent) / 100
+      const endAngle = +startAngle + Number(d.percent) / 100 || 0
+
       paths.push(
         <Path
           color={d.color}
@@ -46,7 +61,7 @@ export function PieChart({ children, radius, data, strokeWidth }: Props) {
     }
 
     return paths
-  }, [radius, innerRadius, strokeWidth, data])
+  }, [radius, innerRadius, strokeWidth, data, theme])
 
   return (
     <Box className="items-center justify-center">
